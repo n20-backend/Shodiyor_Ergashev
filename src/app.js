d3c845c9aef5ec9eddc6e08e0043f74b6cc769e2
Shodiyor Ergashev <shodiyorergashev970@gmail.com>
2025-04-10 22:39:27 +0500

    change

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ import { errorHandler } from "./middlewares/errorHandler.js";
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res, next) => {
+  res.send("Ishlayapti");
+});
+
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/product", productRouter);
 app.use("/api/v1/review", reviewRouter);
